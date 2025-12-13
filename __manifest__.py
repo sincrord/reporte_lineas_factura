8544f763da98198169c91922523de3c253eaaa06
SINCRO Recursos Digitales <93732753+sincrord@users.noreply.github.com>
2025-12-13 10:49:52 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Reporte de Líneas Factura',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Reporte de líneas de factura exportable a XLSX',
     'description': '''
 Reporte de líneas de factura exportable a XLSX.
@@ -15,7 +15,7 @@ Reporte de líneas de factura exportable a XLSX.
         'account',
         # Si en Odoo 19 también tienes este módulo y quieres seguir usando campos como
         # impuesto / tipo_comprobante, descomenta esta línea:
-        # 'cdfi_invoice',
+        'cdfi_invoice',
         #
         # 'stock',        # No se usa en el código actual, se puede dejar fuera
         # 'report_xlsx',  # No se usa el engine de report_xlsx, generamos el archivo manualmente
